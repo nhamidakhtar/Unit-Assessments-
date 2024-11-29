b1cdbb27141f52626b325b0a588164b69068a3c2
nhamidakhtar <nhamidakhtar@gmail.com>
2024-11-29 14:51:24 +0000

    Assessment 5 functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+scale=input("Enter C for celsius and F for fahrenheit:")
+number=float(input"Enter number:"))
+convertf=(number*9/5)+32
+conertc=number-32*(9/5)
+
+if scale== 'C':
+    print(convertf,"F")
+elif scale=='F':
+    print(convertc,"C"
+else:
+    print("invalid scale places put C or F")
