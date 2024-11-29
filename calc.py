35e9f1595eb30290d8e530dfa5c63e913b94e471
nhamidakhtar <nhamidakhtar@gmail.com>
2024-11-29 13:29:34 +0000

    Assesment 2 Variables

---
+++ calc.py
@@ -0,0 +1,7 @@
+num1= int(input("Insert first value:" ))
+num2= int(input("Insert second value2 "))
+
+print("the sum is " ,num1 + num2)
+print("The difference is " ,num1 - num2)
+print("The product is " ,num1 * num2)
+print("The quotient is ",num1 / num2)
