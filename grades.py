b447ba9ca7b5b62b3d2c3b529b209ec0df2a71db
nhamidakhtar <nhamidakhtar@gmail.com>
2024-11-29 14:02:29 +0000

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,19 @@
+mark= int(input( "put the student mark")
+
+if mark>=70 and mark<=100:
+    grade="A"       
+elif mark>= 60 and mark<=70:
+    grade="B"
+elif mark>=50 and mark<=60:
+    grade="C"
+elif mark>=40 and mark<=50:
+    grade="E"
+elif mark>=30 and mark<=40:
+    grade="E"
+elif mark>=20 and makr<=30:
+    grade="F"
+
+else:grade ="invalid makr"
+print(("the grade is:",grade)
+      
+    
