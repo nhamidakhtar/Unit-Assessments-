1601d02fe32ca22434aa8de6f78266c674078521
nhamidakhtar <nhamidakhtar@gmail.com>
2024-12-13 14:28:41 +0000

    Add files via upload

---
+++ Data structures.py
@@ -0,0 +1,27 @@
+my_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+def full_list(my_list):
+    print("list:" , my_list)
+def calculate_sum(my_list):
+    print("The sum of the list is:", sum(my_list))
+
+latgest = max(my_list)
+smallest = min(my_list)
+def largest_number(my_list):
+    print(f"The latgest number of the list is: (largest)
+def smallest_number(my_list):
+    print(f"The smallest number of the list is: (smallest)
+def reverse_list(1st):
+    my_list.reverse()
+    print("List in reverse order:", my_list)
+
+full_list(my_list)
+calculate_sum(my_list)
+largest_number(my_list)
+smallest_number(my_list)
+
+reverse_list(my_list)
+# print again the largest and smallest in the reversed list
+largest_number(my_list)
+smallest_number(my_list)
+    
